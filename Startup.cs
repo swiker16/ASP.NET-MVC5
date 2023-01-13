d1c9fcfc74a309f38d3b3425c26d0b2a01179c7a
swiker16 <101831580+swiker16@users.noreply.github.com>
2023-01-13 19:38:33 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Audi.Startup))]
+namespace Audi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+    }
+}
